4203c6475e8abb5b3752fd6bd75fed6c01b540a1
01b <no.pain.no.code@gmail.com>
2018-11-07 00:50:13 +0900

    class rename

---
+++ src/main/java/ProjectMemberReporter.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+/**
+ * Created by no.pain.no.code@gmail.com
+ * nopainnocode.tistory.com
+ */
+public class ProjectMemberReporter {
+
+    private final ProjectMemberReport projectMemberReport = new ProjectMemberReport();
+    private String line;
+
+    ProjectMemberReporter() {
+    }
+
+    public String getProjectsNameAndCountOfMembers() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (isNextLineExist(scanner))
+            if (isEndOfOneTemplate())
+                projectMemberReport.addProjectAndInitialize();
+            else
+                projectMemberReport.setTemplateUp(line);
+
+        return projectMemberReport.getAllProjectsNameAndCount();
+    }
+
+    private boolean isNextLineExist(Scanner scanner) {
+        return scanner.hasNext()
+                && !(line = scanner.nextLine()).equals("0");
+    }
+
+    private boolean isEndOfOneTemplate() {
+        return "1".equals(line);
+    }
+}
